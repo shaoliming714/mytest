0c2405ea47439450a58fdf1af01dbb559d953fa7
Mr.Shao <649769018@qq.com>
2016-01-24 13:05:37 +0800

    modify

--- MyTest/src/main/java/com/example/mytest/dummy/DummyContent.java
+++ MyTest/src/main/java/com/example/mytest/dummy/DummyContent.java
@@ -27,8 +27,9 @@ public class DummyContent {
         // Add 3 sample items.
         addItem(new DummyItem("1", "Item 1"));
         addItem(new DummyItem("2", "Item 2"));
-        addItem(new DummyItem("3", "Item 3"));
+        addItem(new DummyItem("5", "Item 5"));
         addItem(new DummyItem("4", "Item 4"));
+        addItem(new DummyItem("3", "Item 3"));
     }
 
     private static void addItem(DummyItem item) {
